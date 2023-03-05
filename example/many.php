6592da245dde942b81c983a2bda6ee881b41c7ab
Dennis Iversen <dennis.iversen@gmail.com>
2023-03-05 15:08:41 +0100

    many

---
+++ example/many.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Diversen\ParallelProcess;
+
+$parallel = new ParallelProcess();
+
+// Try with 1000 processes
+for ($i = 0; $i < 1000; $i++) {
+    $parallel->addProcess(function () {
+        return 1;
+    });
+}
+
+$results = $parallel->run();
